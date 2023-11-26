6ca51cccd88d8b531517229cb4dae6d3cb5afc4f
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-11-26 13:05:54 +0600

    firebase connect and signup page create

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,36 +1,72 @@
-import { createContext, useState } from "react";
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut, updateProfile } from 'firebase/auth';
+/* eslint-disable react/prop-types */
+import { createContext, useEffect, useState } from "react";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import app from "../firebase/firebase.config";
-
+import useAxiosPublick from "../Hooks/useAxiosPublick";
 
 
 export const AuthContext=createContext(null)
 
 const auth = getAuth(app);
-const AuthProvider = (({children}) => {
+
+const AuthProvider = ({children}) => {
     const [user,setUser]=useState(null);
     const [loading,setLoading]=useState(true);
+    const googleProvider= new GoogleAuthProvider();
+    const axiosPublic=useAxiosPublick();
 
+    useEffect(()=>{
+       const unsubscribe= onAuthStateChanged(auth,curretnUser=>{
+            setUser(curretnUser)
+            console.log(curretnUser);
+            if (curretnUser) {
+                console.log('get token');
+                const userInfo={
+                    email: curretnUser.email
+                }
+                axiosPublic.post('/jwt', userInfo)
+                .then(res=>{
+                    if (res.data.token) {
+                       localStorage.setItem('access-token', res.data.token); 
+                    }
+                })
+            }
+            else{
+            console.log('remove token');
+            localStorage.removeItem('access-token');
+            }
+            setLoading(false)
+        })
+        return()=>{
+            return unsubscribe();
+        }
+    },[axiosPublic])
     
+    //Log in with Google
+    const googleSignin=()=>{
+        setLoading(true);
+        return signInWithPopup(auth,googleProvider)
+    }
 
      // user created
-     const createUser=(email,password)=>{
+    const createUser=(email,password)=>{
         setLoading(true);
        return createUserWithEmailAndPassword(auth,email,password)
     }
-      // user update profile
-      const updateUserProfile=(name,photo)=>{
-        return updateProfile(auth.currentUser,{
-              displayName:name,
-              photoURL:photo
-          });
-      }
-       //    user Signing in
-      const signIn=(email,password)=>{
+    //    user Signing in
+    const signIn=(email,password)=>{
         setLoading(true);
         return signInWithEmailAndPassword(auth,email,password)
     }
-    // logout
+    // user update profile
+    const updateUserProfile=(name,photo)=>{
+      return updateProfile(auth.currentUser,{
+            displayName:name,
+            photoURL:photo
+        });
+    }
+
+    // User Signing out
     const logOut=()=>{
         setLoading(true);
         return signOut(auth);
@@ -38,13 +74,11 @@ const AuthProvider = (({children}) => {
     const authinfo={
         user,
         loading,
-        // error,
-        // isAuthenticated,
         createUser,
         signIn,
         updateUserProfile,
         logOut,
-        // googleSignin
+        googleSignin
     }
     return (
         <AuthContext.Provider value={authinfo}>
--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 // import App from './App.jsx'
-import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import { HelmetProvider } from "react-helmet-async";
 // import { Root } from 'postcss';
-import Home from './Pages/Home/Home';
-import Root from '../src/Root/Root';
-import ErrorPage from './Pages/ErrorPage/ErrorPage';
-import Login from './Pages/Login/Login';
-import Signup from './Pages/Signup/Signup';
+import Home from "./Pages/Home/Home";
+import Root from "../src/Root/Root";
+import ErrorPage from "./Pages/ErrorPage/ErrorPage";
+import Login from "./Pages/Login/Login";
+import Signup from "./Pages/Signup/Signup";
+import AuthProvider from "./Provider/AuthProvider";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
     errorElement: <ErrorPage></ErrorPage>,
-    children:[
+    children: [
       {
         path: "/",
         element: <Home></Home>,
@@ -30,13 +30,19 @@ const router = createBrowserRouter([
       {
         path: "signup",
         element: <Signup></Signup>,
-      }
-    ] 
+      },
+    ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <AuthProvider>
+      <HelmetProvider>
+        <div className="max-w-screen-xl mx-auto">
+          <RouterProvider router={router} />
+        </div>
+      </HelmetProvider>
+    </AuthProvider>
+  </React.StrictMode>
+);
