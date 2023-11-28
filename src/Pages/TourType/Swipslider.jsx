ded4bbe7136ec7e42531552ca2239d22bdf45a16
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-11-28 16:31:09 +0600

    Tour type added

--- src/Pages/TourType/Swipslider.jsx
+++ src/Pages/TourType/Swipslider.jsx
@@ -1,11 +1,16 @@
-import { FaBaby } from "react-icons/fa6";
 
-const Swipslider = () => {
+const Swipslider = ({heading,icon}) => {
     return (
-        <div className="border-spacing-3 border-white bg-red-600 h-40 rounded-full w-40 text-center items-center text-4xl mx-auto text-white">
-            <FaBaby></FaBaby>
-           Sports
-        </div>
+        <div
+        style={{ border: "2px solid white" }}
+        className="border-spacing-3 border-white  h-40 rounded-full w-40 text-center items-center text-4xl mx-auto text-white cursor-pointer hover:text-black hover:bg-white"
+      >
+        <p className="text-center mx-auto flex justify-center pt-4">
+          {icon}
+          
+        </p>
+        <p className="lg:pt-4">{heading}</p>
+      </div>
     );
 };
 
--- src/Pages/TourType/TourType.jsx
+++ src/Pages/TourType/TourType.jsx
@@ -1,48 +1,71 @@
-
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-// import required modules
-import backgroundImg from '../../assets/tourbgimg.jpg'
-import Swipslider from './Swipslider';
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import backgroundImg from "../../assets/tourbgimg.jpg";
+import { FaBagShopping, FaCat, FaPersonWalking, FaShapes, FaWheelchair, FaWind } from "react-icons/fa6";
+import Swipslider from "./Swipslider";
 
 const TourType = () => {
-    return (
-        <div className='lg:h-96' style={{ backgroundImage: `url(${backgroundImg})` }}>
-            <div className='text-center'>
-            <h2 className='text-white '>Find a tour</h2>
-            <h2 className='text-5xl my-5 text-white font-bold'>Tour Type</h2>
-            </div>
-            <hr className="border-white dark:border-white mx-10"></hr>
-            <>
-      <h3>
-        
-      </h3>{" "}
-      <br />
-      <div className='mx-16'>
-      <Swiper watchSlidesProgress={true} slidesPerView={3} className="mySwiper">
-        <SwiperSlide>
-            <Swipslider></Swipslider>
-        </SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper>
+
+  return (
+    <div className="relative lg:h-96">
+      <div
+        className="absolute inset-0"
+        style={{
+          backgroundImage: `url(${backgroundImg})`,
+          backgroundSize: "cover",
+          opacity: 0.5, // Adjust the opacity as needed
+          backgroundColor: "rgba(0, 0, 0, 0.966)", // Overlay color
+        }}
+      ></div>
+      <div className="relative text-center z-20">
+        <h2 className="text-white pt-7">Find a tour</h2>
+        <h2 className="text-5xl my-5 text-white font-bold">Tour Type</h2>
       </div>
-    </>
+      <hr className="border-white dark:border-white mx-10"></hr>
+      <>
+        <h3></h3>
+        <br />
+        <div className="mx-16">
+          <Swiper
+            watchSlidesProgress={true}
+            slidesPerView={3}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+        <Swipslider heading={'Sports'} icon={<FaWheelchair></FaWheelchair>}>
+        </Swipslider>
+            </SwiperSlide>
+
+            <SwiperSlide>
+            <Swipslider heading={'Walking'} icon={<FaPersonWalking></FaPersonWalking>}>
+          </Swipslider>   
+            </SwiperSlide>
+            <SwiperSlide>
+              {" "}
+              <Swipslider heading={'WildLife'} icon={<FaCat></FaCat>}>
+        </Swipslider>
+            </SwiperSlide>
+            <SwiperSlide>
+              {" "}
+              <Swipslider heading={'Air Rides'} icon={<FaWind></FaWind>}>
+        </Swipslider>
+            </SwiperSlide>
+            <SwiperSlide>
+              {" "}
+              <Swipslider heading={'Cruises'} icon={<FaShapes></FaShapes>}>
+        </Swipslider>
+            </SwiperSlide>
+            <SwiperSlide>
+              {" "}
+              <Swipslider heading={'Hiking'} icon={<FaBagShopping></FaBagShopping>}>
+        </Swipslider>
+            </SwiperSlide>
+          </Swiper>
         </div>
-    );
+      </>
+    </div>
+  );
 };
 
 export default TourType;
