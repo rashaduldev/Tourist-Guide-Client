7d57bb764311fd0e8402e29bf308c5295cf9721a
rashaduldev <rashadul.dev@gmail.com>
2024-06-03 16:32:44 +0600

    server link change

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 
 
 const axiosSecure=axios.create({
-    baseURL:'https://tourist-guide-server-blush.vercel.app/'
+    baseURL:'https://tourist-guide-server-blush.vercel.app'
 })
 const useAxiosSecure = () => {
     // const {logOut}=useAuth();
